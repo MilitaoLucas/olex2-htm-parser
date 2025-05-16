cfe2a96145caeea969d0520a7760b656d5bec689
LucasMilitao <lucas.milito@usp.br>
2025-05-15 22:14:33 -0300

    managed to kind of fix comments, still needs work

--- grammar.js
+++ grammar.js
@@ -11,30 +11,51 @@ const HTML = require('./tree-sitter-html/grammar');
 
 module.exports = grammar(HTML, {
   name: "html", // Or your custom grammar name like "olex2htm"
-  
+
   extras: HTML.extras,
 
-  inline: $ => HTML.inline ? HTML.inline($) : [], 
+  inline: $ => HTML.inline ? HTML.inline($) : [],
   conflicts: $ => HTML.conflicts ? HTML.conflicts($) : [],
 
   rules: {
     ...HTML.rules,
-    comment: $ => seq(
+
+    _node: $ => choice(
+      $.doctype,
+      $.entity,
+      $.text,
+      $.element,
+      $.script_element,
+      $.style_element,
+      $.comment,
+      $.include_keyword,
+    ),
+
+    // Override the comment rule
+    comment: $ => choice(
+      $.include_comment,
+      $.normal_comment
+    ),
+
+    normal_comment: $ => seq(
       '<!--',
-      repeat($._custom_comment_item),
-      '-->'),
-    _custom_comment_item: $ => choice(
-      $.custom_directive_block,
-      $.custom_plain_comment_segment
+      optional(/\s*?/),
+      seq(
+        /[^#]/,
+        optional($._text)),
+      '-->'
+    ),
+    // Special case for include comments
+    include_comment: $ => seq(
+      '<!--',
+      optional(/\s*?/),
+      $.include_keyword,
+      optional(/\s*?/),
+      optional($._text),
+      '-->'
     ),
 
-    custom_directive_block: $ => seq(
-      $.custom_directive_name,
-      repeat($.custom_directive_argument_text)
-    ),
-
-    custom_directive_name: $ => /#[\w\.]+/,
-    custom_directive_argument_text: $ => token(prec(-1, /[^#]+/)),
-    custom_plain_comment_segment: $ => token(prec(-2, /[^-<>-]+/)),
-  },
+    include_keyword: $ => '#include',
+    _text: (_) => /[^-^-]*?/,
+  }
 });
